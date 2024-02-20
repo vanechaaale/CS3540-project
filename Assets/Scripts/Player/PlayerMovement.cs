4c6b56116bec6c0220d80079b142c37cc3404617
Jazmyn Harris <jazmynrh12@gmail.com>
2024-02-20 00:55:42 -0500

    creating level 1

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float playerSpeed = 1f;
+    public float playerSpeed = 5f;
+
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,8 +22,10 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal") * playerSpeed * 0.25f;
-        float moveVertical = Input.GetAxis("Vertical") * playerSpeed * 0.25f;
+        float moveHorizontal = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;   
+
+        transform.LookAt(rb.velocity);
 
         transform.position += new Vector3(moveHorizontal, 0f, moveVertical);
     }
