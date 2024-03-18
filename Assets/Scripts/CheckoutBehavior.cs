ce27791479a93dc2759d2c27d311507c2f9da9f6
Jazmyn Harris <jazmynrh12@gmail.com>
2024-03-18 13:46:10 -0400

    implemented checkout behavior

--- Assets/Scripts/CheckoutBehavior.cs
+++ Assets/Scripts/CheckoutBehavior.cs
@@ -4,48 +4,68 @@ using UnityEngine;
 
 public class CheckoutBehavior : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public List<string> customerList;
     public float checkoutTimer = 0.5f;
     public float minDistance = 1f;
-    float checkoutCountdown = 0.5;
+    float checkoutCountdown = 0.5f;
+    public List<string> itemList;
 
     // Start is called before the first frame update
     void Start()
     {
         if (player == null) {
-            player = GameObject.FindGameObjectWithTag("Player").transform;
+            player = GameObject.FindGameObjectWithTag("Player");
         }
+        customerList = new List<string>();
+        itemList = new List<string>();
         GetNextCustomer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dist = Vector3.Distance(transform.position, player);
+        float dist = Vector3.Distance(transform.position, player.transform.position);
         if (dist <= minDistance) {
             CheckoutCustomer();
         }
         if (customerList.Count == 0) {
             GetNextCustomer();
-        }
+        } 
     }
 
     void CheckoutCustomer() {
+
+        Debug.Log(itemList);
+
         if (checkoutCountdown <= 0) {
-            checkoutCountdown = checkoutTimer;
-        } else {
-            for (int i = customerList.Count - 1; i  >= 0; i--) {
-                if (ItemCollection.itemList.Contains(customerList.Get(i))) {
-                    ItemCollection.itemList.Remove(customerList.Get(i));
-                    FindObjectOfType<ItemCollection>().removeFromList(customerList.Get(i));
+            // check if the customer's item is found in the player's basket
+            if (ItemCollection.itemList.Contains(customerList[0])) {
+                
+                // remove the item from both customer and player's list if found
+                ItemCollection.itemList.Remove(customerList[0]);
+                FindObjectOfType<ItemCollection>().removeFromList(customerList[0]);
+
+                if (customerList.Count > 0) {
+                    // remove the customer from the Shopping list
+                    FindObjectOfType<CustomerManagerBehavior>().RemoveCustomer();
+                    // TODO: INCREMENT SCORE
                 }
+
+                // restart the countdown timer
+                checkoutCountdown = checkoutTimer;
             }
+            
+        } else if (customerList.Count > 0) {
+            // decrease the timer for the item being checked out
             checkoutCountdown -= Time.deltaTime;
         }
     }
 
     void GetNextCustomer() {
-        customerList = FindObjectOfType<CustomerManager>().GetComponent<CustomerManagerBehavior>().customerList.Get(0);
+        List<List<string>> custList = GameObject.Find("CustomerManager").GetComponent<CustomerManagerBehavior>().customerList;
+        if (custList != null && custList.Count > 0 ) {
+            customerList = custList[0];
+        }
     }
 }
